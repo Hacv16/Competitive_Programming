728b149c6b8f23056f433fa94fcbd988d9c7ddf4
ThegeekKnight16 <123429133+ThegeekKnight16@users.noreply.github.com>
2023-01-24 02:38:13 +0000

    haha

--- SegPersistenteTemplate(Ponteiro).cpp
+++ SegPersistenteTemplate(Ponteiro).cpp
@@ -7,12 +7,12 @@
 using namespace std;
 struct SegPersistente
 {
-    int quant, marc;
+    int quant;
     int ini, fim;
     SegPersistente* e;
     SegPersistente* d;
 
-    SegPersistente(int Q = 0, int M = 0, int INI = 0, int FIM = 0) {quant = Q; marc = 0; ini = INI; fim = FIM;}
+    SegPersistente(int Q = 0, int INI = 0, int FIM = 0) {quant = Q; ini = INI; fim = FIM; e = this; d = this;}
 
     SegPersistente* update(int id, int val)
     {
@@ -20,36 +20,39 @@ struct SegPersistente
         *novo = *this;
         if (ini == fim)
         {
-            quant += val;
-            if (quant == 0) marc = 0;
-            else if (quant == 1) marc = 1;
+            quant = val;
+            // if (quant == 0) marc = 0;
+            // else if (quant == 1) marc = 1;
             return novo;
         }
         int m = (ini + fim) >> 1;
         if (id <= m)
         {
+            //Cuidado no c++11, da problema
             novo->e = e->update(id, val);
         }
         else
         {
+            //Cuidado no c++11, da problema
             novo->d = d->update(id, val);
         }
         novo->quant = (novo->e)->quant + (novo->d)->quant;
-        novo->marc  = (novo->e)->marc  + (novo->d)->marc ;
+        // novo->marc  = (novo->e)->marc  + (novo->d)->marc ;
         return novo;
     }
 
     int query(int p, int q)
     {
         if (q < ini || p > fim) return 0;
-        if (p <= ini && fim <= q) return marc;
+        if (p <= ini && fim <= q) return quant;
         int m = (ini + fim) >> 1;
         return (e->query(p, q) + d->query(p, q));
     }
 };
 SegPersistente nulo(0, 0, 0);
-const int MAXN = 3e4 + 10;
-SegPersistente* raiz[MAXN];
+const int MAXN = 1e6 + 10;
+vector<SegPersistente*> raiz;
+int lastX[MAXN];
 
 int main()
 {
@@ -63,8 +66,16 @@ int main()
     {
         int x;
         cin >> x;
-        raiz[i] = raiz[i-1].update(x, +1);
+        raiz[i] = raiz[i-1]->update(lastX[x], 0);
+        raiz[i] = raiz[i]->update(i, +1);
+        lastX[x] = i;
     }
     int Q;
     cin >> Q;
+    for (int q = 1; q <= Q; q++)
+    {
+        int X, Y;
+        cin >> X >> Y;
+        cout << raiz[Y]->query(X, Y) << '\n';
+    }
 }
