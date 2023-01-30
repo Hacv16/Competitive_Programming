c6d4b66bbcebf2c4f9a6f5eaf4ae42991e279fa8
Hacv16 <123428940+Hacv16@users.noreply.github.com>
2023-01-30 19:50:13 -0300

    Create BIT_2D.cpp

---
+++ Estruturas_de_Dados/BIT_2D.cpp
@@ -0,0 +1,20 @@
+void update(int x, int y, int v){
+    for(int i = x; i <= n; i += (i & -i))
+        for(int j = y; j <= n; j += (j & -j))
+            BIT[i][j] += v;
+}
+ 
+int sum(int x, int y){ // A function to get sum from (0, 0) to (x, y)
+    int s = 0;
+ 
+    for(int i = x; i > 0; i -= (i & -i))
+        for(int j = y; j > 0; j -= (j & -j))
+            s += BIT[i][j];
+ 
+    return s;
+}
+ 
+int query(int x1, int y1, int x2, int y2){  //rectangle query
+    return sum(x2, y2) - sum(x2, y1 - 1) - sum(x1 - 1, y2) + sum(x1 - 1, y1 - 1);
+}
+ 
