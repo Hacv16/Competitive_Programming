fdc3170236cce1017f751c276f480870e47093c6
Hacv16 <123428940+Hacv16@users.noreply.github.com>
2023-01-31 15:06:13 -0300

    Update Busca_Ternaria.cpp

--- Buscas/Busca_Ternaria.cpp
+++ Buscas/Busca_Ternaria.cpp
@@ -1,11 +1,10 @@
-while(r - l >= 3){
-    ll m1 = l + (r - l) / 3;
-    ll m2 = r - (r - l) / 3;
-    if(f(m1) > f(m2)) r = m2;
-    else l = m1;
-}
-
-ll ans = -INF;
-
-for(int i = l; i <= r; i++)
-    ans = max(ans, f(i));
+    while(r - l > 3){
+        ll m1 = l + (r - l) / 3;
+        ll m2 = r - (r - l) / 3;
+ 
+        if(f(m1) < f(m2)) r = m2;
+        else l = m1;
+    }
+ 
+    for(ll i = l; i <= r; i++)
+        ans = min(ans, f(i));
