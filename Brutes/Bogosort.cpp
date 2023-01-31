32d8582ab3854b02d0e72242312fcf54e4901992
Hacv16 <123428940+Hacv16@users.noreply.github.com>
2023-01-31 07:30:21 -0300

    Create Bogosort.cpp

---
+++ Brutes/Bogosort.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	srand(time(0));
+
+	int n; cin >> n;
+	vector<int> v(n);
+
+	for(auto &x : v) cin >> x;
+
+	while(!is_sorted(v.begin(), v.end()))
+		random_shuffle(v.begin(), v.end());
+
+	for(auto x : v)
+		cout << x << ' ';
+}
