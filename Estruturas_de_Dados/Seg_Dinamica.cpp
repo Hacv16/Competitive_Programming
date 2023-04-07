db251b8e8df62d1264582423c490eecbb1f2094c
Hacv16 <123428940+Hacv16@users.noreply.github.com>
2023-04-07 20:15:54 -0300

    Update Seg_Dinamica.cpp
    better version bonga

--- Estruturas_de_Dados/Seg_Dinamica.cpp
+++ Estruturas_de_Dados/Seg_Dinamica.cpp
@@ -1,42 +1,55 @@
-class sparseSeg{
+class SparseSeg{
 	public:
 		int create(void){
-			seg.push_back(0);
-			getL.push_back(-1);
-			getR.push_back(-1);
+			seg.emplace_back(0);
+			getL.emplace_back(0);
+			getR.emplace_back(0);
 			return seg.size() - 1;
 		}
 
 		void initiate(){ create(); create(); }
 
-		void update(ll i, ll x, ll p, ll l, ll r){
+		int getLeft(int p){
+			if(getL[p] == 0){ int aux = create(); getL[p] = aux; }
+			return getL[p];
+		}
+
+		int getRight(int p){
+			if(getR[p] == 0){ int aux = create(); getR[p] = aux; }
+			return getR[p];
+		}
+
+		void update(int i, int x, int p, int l, int r){
 			if(i < l || i > r) return;
 			if(l == r){
-				seg[p] += x;
+				seg[p] = x;
 				return;
 			}
 
-			ll m = (l + r) >> 1;
+			int m = (l + r) >> 1;
 
-			if(getL[p] == -1){ int aux = create(); getL[p] = aux; }
-			update(i, x, getL[p], l, m);
-
-			if(getR[p] == -1){ int aux = create(); getR[p] = aux; } 
-			update(i, x, getR[p], m + 1, r);
+			update(i, x, getLeft(p), l, m);
+			update(i, x, getRight(p), m + 1, r);
 
 			seg[p] = seg[ getL[p] ] + seg[ getR[p] ];
 		}
 
-		ll query(ll a, ll b, ll p, ll l, ll r){
-			if(a > r || b < l) return 0;
+		int query(int a, int b, int p, int l, int r){
+			if(a > r || b < l || p == 0) return 0;
 			if(a <= l && r <= b) return seg[p];
 
-			ll m = (l + r) >> 1, ans = 0;
+			int m = (l + r) >> 1;	
+			return query(a, b, getL[p], l, m) + query(a, b, getR[p], m + 1, r);
+		}
 
-			if(getL[p] != -1) ans += query(a, b, getL[p], l, m);
-			if(getR[p] != -1) ans += query(a, b, getR[p], m + 1, r);
+		int bbin(int x, int p, int l, int r){
+			if(seg[p] < x) return -1;
 
-			return ans;
+			if(l == r) return l;
+			int m = (l + r) >> 1;
+
+			if(seg[ getL[p] ] >= x) return bbin(x, getL[p], l, m);
+			return bbin(x - seg[ getL[p] ], getR[p], m + 1, r);
 		}
 
 	protected:
