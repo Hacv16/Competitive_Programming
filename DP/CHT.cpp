76c24b9a63e4233a632d9da092e247645833b5fa
Hacv16 <123428940+Hacv16@users.noreply.github.com>
2023-09-14 18:54:40 -0300

    Update CHT.cpp
    change

--- DP/CHT.cpp
+++ DP/CHT.cpp
@@ -1,25 +1,26 @@
-bool inutil(ll i, ll j, ll k){
-    return (ld) (b[k] - b[i]) / (m[i] - m[k]) <= (ld) (b[j] - b[i]) / (m[i] - m[j]);
+inline int f(int i, int x){ return m[i] * x + b[i]; }
+ 
+inline bool inutil(int i, int j, int k)
+{
+	int l = (b[k] - b[i]) * (m[i] - m[j]);
+	int r = (b[j] - b[i]) * (m[i] - m[k]);
+	return l <= r;
 }
  
-void add(ll a, ll k){ //add y = ax + k
-    m.push_back(a);
-    b.push_back(k);
+inline void add(int a, int k) //add ax + k
+{
+	m.push_back(a); b.push_back(k);
  
-    while(sz(m) >= 3 && inutil(m.size() - 3, m.size() - 2, m.size() - 1)){
-        m.erase(m.end() - 2);
-        b.erase(b.end() - 2);
-    }
+	while(m.size() >= 3 && inutil(sz(m) - 3, sz(m) - 2, sz(m) - 1))
+	{
+		m.erase(m.end() - 2);
+		b.erase(b.end() - 2);
+	}
 }
  
-ll f(ll i, ll x){
-    return m[i] * x + b[i];
-}
- 
-ll query(ll x){
-    if (ptr >= sz(m)) ptr = sz(m) - 1;
-    while (ptr < sz(m) - 1 && f(ptr + 1, x) <= f(ptr, x)) ++ptr;
- 
-    return f(ptr, x);
-}
- 
+int query(int x)
+{
+	if(p >= m.size()) p = m.size() - 1;
+	while(p < m.size() - 1 && f(p + 1, x) < f(p, x)) p++;
+	return f(p, x);
+}	
