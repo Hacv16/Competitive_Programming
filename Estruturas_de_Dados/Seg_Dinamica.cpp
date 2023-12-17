e7c1495d87c8e7d29f56c505e8305489a3c91d86
Hacv16 <123428940+Hacv16@users.noreply.github.com>
2023-12-17 08:10:03 -0300

    Update Seg_Dinamica.cpp

--- Estruturas_de_Dados/Seg_Dinamica.cpp
+++ Estruturas_de_Dados/Seg_Dinamica.cpp
@@ -1,30 +1,41 @@
-class SparseSeg{
+class SparseSeg
+{
 	public:
-		int create(void){
-			seg.emplace_back(0);
-			getL.emplace_back(0);
-			getR.emplace_back(0);
-			return seg.size() - 1;
+		int create(void)
+		{
+			int id = seg.size();
+
+			seg.push_back(INF);
+			getL.push_back(0);
+			getR.push_back(0);
+
+			return id;
 		}
 
-		void setUp(){ 
+		void setUp()
+		{ 
 			seg.clear(); getL.clear(); getR.clear();
 			create(); create(); 
 		}
 
-		int getLeft(int p){
+		int getLeft(int p)
+		{
 			if(getL[p] == 0){ int aux = create(); getL[p] = aux; }
 			return getL[p];
 		}
 
-		int getRight(int p){
+		int getRight(int p)
+		{
 			if(getR[p] == 0){ int aux = create(); getR[p] = aux; }
 			return getR[p];
 		}
 
-		void update(int i, int x, int p, int l, int r){
+		void update(int i, int x, int p, int l, int r)
+		{
 			if(i < l || i > r) return;
-			if(l == r){
+			
+			if(l == r)
+			{
 				seg[p] = x;
 				return;
 			}
@@ -34,27 +45,18 @@ class SparseSeg{
 			update(i, x, getLeft(p), l, m);
 			update(i, x, getRight(p), m + 1, r);
 
-			seg[p] = seg[ getL[p] ] + seg[ getR[p] ];
+			seg[p] = min(seg[ getLeft(p) ], seg[ getRight(p) ]);
 		}
 
-		int query(int a, int b, int p, int l, int r){
-			if(a > r || b < l || p == 0) return 0;
+		int query(int a, int b, int p, int l, int r)
+		{
+			if(a > r || b < l || p == 0) return INF;
 			if(a <= l && r <= b) return seg[p];
 
 			int m = (l + r) >> 1;	
-			return query(a, b, getL[p], l, m) + query(a, b, getR[p], m + 1, r);
-		}
-
-		int bbin(int x, int p, int l, int r){
-			if(seg[p] < x) return -1;
-
-			if(l == r) return l;
-			int m = (l + r) >> 1;
-
-			if(seg[ getL[p] ] >= x) return bbin(x, getL[p], l, m);
-			return bbin(x - seg[ getL[p] ], getR[p], m + 1, r);
+			return min(query(a, b, getL[p], l, m), query(a, b, getR[p], m + 1, r));
 		}
 
 	protected:
-		vector<ll> seg, getL, getR;
+		vector<int> seg, getL, getR;
 };
